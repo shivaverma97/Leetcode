ffeabd42f88ae65037887b4a43222b5c9b70f3a8
shivaverma97 <shiva.verma9747@gmail.com>
2023-04-19 12:22:21 +0530

    compare two binary trees

---
+++ compare_binary_tree.js
@@ -0,0 +1,29 @@
+// Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+// Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    if(p == null && q == null){
+        return true
+    }
+
+    if(p != null && q != null){
+        return (p.val == q.val &&
+            isSameTree(p.left, q.left) &&
+            isSameTree(p.right, q.right))
+    }
+    return false
+};
