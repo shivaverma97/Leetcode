43860733595b7d16fe0090dc47136ef1cfd7b459
shivaverma97 <shiva.verma9747@gmail.com>
2023-04-19 16:50:22 +0530

    palindrome

---
+++ palindrome_without_alphanumerics.js
@@ -0,0 +1,17 @@
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s = s.replace(/[^a-zA-Z0-9]/g, '');
+    s = s.toLowerCase()
+    reversed = s.split('').reverse().join('')
+    if(s === reversed){
+        return true
+    }
+    return false
+};
