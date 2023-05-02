0b41bc3c83b86f16a6b7eb7b229e2f8651121b83
shivaverma97 <shiva.verma9747@gmail.com>
2023-05-02 17:42:47 +0530

    updated logic to pass all test cases

--- balanced_binaryTree.js
+++ balanced_binaryTree.js
@@ -15,52 +15,24 @@
  * @return {boolean}
  */
 var isBalanced = function(root) {
-    let left_subtree_node
-    let left_subtree_depth = 0
-    let right_subtree_node
-    let right_subtree_depth = 0
-
-    if(root === null){
-        return true
-    }
-
-    if(root.left !== null){
-        left_subtree_node = root.left
-        left_subtree_depth = depthOfTree(left_subtree_node)
-    }
-
-    if (root.right !== null){
-        right_subtree_node = root.right
-        right_subtree_depth = depthOfTree(right_subtree_node)
-    }
-
-    if(left_subtree_depth - right_subtree_depth > 1){
-        return false
-    }
-
-    return true
-};
-
-function depthOfTree(node){
-    let depth = 0
-    stack = []
-    stack.push(node)
-
-    while(stack.length > 0){
-        let size = stack.length
-
-        for(let i = 0; i<size; i++){
-            let temp = stack.shift()
-            if(temp.left !== null){
-                stack.push(temp.left)
-            }
-            if(temp.right != null){
-                stack.push(temp.right)
-            }
-        }
-
-        depth = depth + 1
-    }
-
-    return depth
+    // If the tree is empty, we can say it’s balanced...
+    if (root == null)  return true;
+    // Height Function will return -1, when it’s an unbalanced tree...
+	if (Height(root) == -1)  return false;
+	return true;
 }
+// Create a function to return the “height” of a current subtree using recursion...
+var Height = function(root) {
+    // Base case...
+	if (root == null)  return 0;
+    // Height of left subtree...
+	let leftHeight = Height(root.left);
+    // Height of height subtree...
+	let rightHight = Height(root.right);
+    // In case of left subtree or right subtree unbalanced, return -1...
+	if (leftHeight == -1 || rightHight == -1)  return -1;
+    // If their heights differ by more than ‘1’, return -1...
+    if (Math.abs(leftHeight - rightHight) > 1)  return -1;
+    // Otherwise, return the height of this subtree as max(leftHeight, rightHight) + 1...
+	return Math.max(leftHeight, rightHight) + 1;
+};
