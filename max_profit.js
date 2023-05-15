6dc76d1b0e66e467b541e5c6afbc0fbbbd5a846a
shivaverma97 <shiva.verma9747@gmail.com>
2023-05-15 15:03:11 +0530

    solved max profit

---
+++ max_profit.js
@@ -0,0 +1,37 @@
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+// Example 1:
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let right = 0
+    let left = 0
+    let max_profit = 0
+
+    while(right<prices.length){
+        if(prices[left] < prices[right]){
+            let profit = prices[right] - prices[left]
+            max_profit = profit > max_profit ? profit : max_profit
+        }
+        else{
+            left = right
+        }
+        right++
+    }
+
+    return max_profit
+};
