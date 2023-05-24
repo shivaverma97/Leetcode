9e325301544d2a412e8a304c3853cc7a43149a0c
shivaverma97 <shiva.verma9747@gmail.com>
2023-05-24 12:04:01 +0530

    solved two sum

---
+++ two_sum.js
@@ -0,0 +1,30 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = new Map()
+
+    for(let i =0; i<nums.length; i++){
+        let complement = target - nums[i]
+        if(map.has(complement)){
+            return [i, map.get(complement)]
+        }
+        map.set(nums[i], i)
+    }
+};
